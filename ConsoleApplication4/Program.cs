4f43b94c5c5a1f8cd230133f76550d98159e4d30
qq1269832742 <1269832742@qq.com>
2017-08-01 16:55:29 +0800

    code over!--xww

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MaxValue;  // 2147483647
+
+            int min = int.MinValue; // -2147483648
+
+            unchecked
+            {
+
+                max += max;
+
+            }
+        }
+    }
+}
